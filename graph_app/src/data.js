60aaf214881dfc3cd43d02b0489ac37d58d7d843
Antoine <a.ruchot@outlook.com>
2022-06-21 22:06:18 +0200

    creation du jeu de doonées

---
+++ graph_app/src/data.js
@@ -0,0 +1,37 @@
+export const userData = [
+    {
+        id: 1,
+        year: '2015',
+        nbUser : 1000
+    },
+    {
+        id: 2,
+        year: '2016',
+        nbUser : 480
+    },
+    {
+        id: 3,
+        year: '2017',
+        nbUser : 2400
+    },
+    {
+        id: 4,
+        year: '2018',
+        nbUser : 2200
+    },
+    {
+        id: 5,
+        year: '2019',
+        nbUser : 2950
+    },
+    {
+        id: 6,
+        year: '2020',
+        nbUser : 1200
+    },
+    {
+        id: 7,
+        year: '2021',
+        nbUser : 605
+    }
+]
